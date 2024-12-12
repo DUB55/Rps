c29820520844b14f05d4c8daf7e9c442c9ef1e36
DUB55 <tcookdub5@gmail.com>
2024-12-12 14:43:37 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "https://your-github-pages-url.github.io",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
